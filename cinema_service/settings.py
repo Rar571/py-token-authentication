6d24c4e725f5099aad0994a82f3cb425c5f04d86
Ruslan <stadnikruslan168@gmail.com>
2026-04-01 21:08:30 +0300

    Solution

--- cinema_service/settings.py
+++ cinema_service/settings.py
@@ -145,5 +145,6 @@ REST_FRAMEWORK = {
 ),
 "DEFAULT_PERMISSION_CLASSES": (
 "rest_framework.permissions.AllowAny",
+"rest_framework.permissions.IsAuthenticated",
 ),
 }
--- user/serializers.py
+++ user/serializers.py
@@ -9,7 +9,7 @@ class UserSerializer(serializers.ModelSerializer):
         fields = ("id", "username", "email", "password", "is_staff")
         read_only_fields = ("id", "is_staff")
         extra_kwargs = {
-            "password": {"write_only": True, "min_length": 6}
+            "password": {"write_only": True, "min_length": 5}
         }
 
     def create(self, validated_data):
