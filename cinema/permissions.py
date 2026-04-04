60dae6667e3070b6ff2382a785c6774267be57ab
Ruslan <stadnikruslan168@gmail.com>
2026-04-04 16:16:19 +0300

    Solution

--- cinema/permissions.py
+++ cinema/permissions.py
@@ -1,12 +1,14 @@
-from rest_framework.permissions import BasePermission
+from rest_framework.permissions import BasePermission, SAFE_METHODS
 
 
+from rest_framework.permissions import BasePermission, SAFE_METHODS
 class IsAdminOrIfAuthenticatedReadOnly(BasePermission):
-
     def has_permission(self, request, view):
-        if request.user.is_staff:
-            return True
-        if (request.user.is_authenticated
-                and request.method in ("GET", "HEAD", "OPTIONS")):
-            return True
-        return False
+        return bool(
+            (
+                request.method in SAFE_METHODS
+                and request.user
+                and request.user.is_authenticated
+            )
+            or (request.user and request.user.is_staff)
+        )
